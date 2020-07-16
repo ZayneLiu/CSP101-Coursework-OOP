aa74c4f76dd51f30a66d25b66a60fdc959e3db7f
Zayne.Liu <llz981011@hotmail.com>
2020-07-17 00:21:34 +0100

    Class fields
    1.1 - CD Class
    1.2 - DVD Class

---
+++ src/com/zayne/coursework/CD.java
@@ -0,0 +1,18 @@
+package com.zayne.coursework;
+
+public class CD {
+
+    /** The title of the CD. */
+    public String title;
+    /** The artist of the CD. */
+    public String artist;
+    /** The number of tracks on the CD. */
+    public int numOfTracks;
+    /** The running time of the CD. */
+    public int playingTime;
+    /** Indicates whether the CD is available. */
+    public boolean isAvailable;
+    /** The comments on the CD. */
+    public String comments;
+
+}
---
+++ src/com/zayne/coursework/DVD.java
@@ -0,0 +1,16 @@
+package com.zayne.coursework;
+
+public class DVD {
+
+    /** The title of the DVD. */
+    public String title;
+    /** The director of the DVD. */
+    public String director;
+    /** The running time of the DVD. */
+    public int playingTime;
+    /** Indicates whether the DVD is available. */
+    public boolean isAvailable;
+    /** Comments on the DVD. */
+    public String comments;
+    
+}
